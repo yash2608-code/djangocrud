dbc23c0c62252d9b9b1ff84b4e27f7b9ad93473f
Yash <yashpoojara268@gmail.com>
2021-05-17 16:11:34 +0530

    login and session done

--- app/urls.py
+++ app/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path("",views.FirstPage,name="firstpage"),
+    path("firstpage/",views.FirstPage,name="firstpage"),
     path("second-page/",views.SecondPage,name="secondpage"),
     path("show-data/",views.ShowData,name="showdata"),
     path("update-page/<int:pk>/",views.UpdatePage,name="updatepage"),
     path("delete-user/<int:pk>/",views.DelUser,name="deluser"),
     path("reg-page/",views.RegPage,name="regpage"),
+    path("",views.LoginPage,name="loginpage"),
     path("reg-user/",views.RegUser,name = "reguser"),
+    path("loguser/",views.LoginUser,name = "loguser"),
+    path("logout/",views.Logout,name="logout"),
     path("update-user/<int:pk>/",views.UpdateUser,name="updateuser"),
 ]
--- app/views.py
+++ app/views.py
@@ -10,9 +10,15 @@ def SecondPage(request):
 def RegPage(request):
     return render(request, "app/reg.html")
 
+def LoginPage(request):
+    return render(request, "app/login.html")
+
 def ShowData(request):
-    data = User.objects.all()
-    return render(request, "app/show.html",{'user':data})
+    if 'id' in request.session and 'email' in request.session:
+        data = User.objects.all()
+        return render(request, "app/show.html",{'user':data})
+    else:
+        return redirect("/")
 
 def UpdatePage(request,pk):
     data = User.objects.get(id=pk)
@@ -46,3 +52,33 @@ def DelUser(request,pk):
     data = User.objects.get(id=pk)
     data.delete()
     return redirect('showdata')
+
+def LoginUser(request):
+    email = request.POST['email']
+    password = request.POST['passwd']
+    print(f"Login Credentials---------->{email}--{password}")
+
+    user = User.objects.filter(Email=email)
+    print(f"--------->GOT USER FOR LOGIN---------->{user}")
+
+    if len(user)>0:
+        if user[0].password == password:
+            request.session['id'] = user[0].id
+            request.session['firstname'] = user[0].password
+            request.session['email'] = user[0].Email
+            return redirect("showdata")
+        else:
+            msg = "Password is incorrect"
+            return render(request, "app/login.html",{'err':msg})
+
+    else:
+        msg = "User Doesn't Found"
+        return render(request, "app/login.html",{'err':msg})
+
+    return redirect("/")
+
+def Logout(request):
+    del request.session['id']
+    del request.session['firstname']
+    del request.session['email']
+    return redirect("/")
